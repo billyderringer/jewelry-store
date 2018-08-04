3b91f733bbde0fa5acff0c9a50fab9cfca15467b
Billy Derringer <billyderringer7@gmail.com>
2018-08-03 21:00:24 -0500

    added navbar transition

--- config.js
+++ config.js
@@ -4,5 +4,6 @@ requirejs.config({
             'https://code.jquery.com/jquery-3.3.1.min',
             'js/jQuery-3.3.1.min'
         ],
+        styles: 'js/styles'
     }
 })
---
+++ js/styles.js
@@ -0,0 +1,15 @@
+define(['jquery'], () => {
+    $(window).scroll(function() { // check if scroll event happened
+        if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
+            $("#floating-nav").css({
+                "background-color": "#151011",
+                "transition": "1s"
+            }); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+        } else {
+            $("#floating-nav").css({
+                "background-color": "transparent",
+                "transition": "1s"
+            }); // if not, change it back to transparent
+        }
+    })
+})
